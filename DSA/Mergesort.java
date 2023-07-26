3148c1c4847a9dc48b953bc2c15205c010f61858
BMCBim <something123sdhgk@proton.me>
2023-07-26 14:29:59 +0545

    incomplete merge sort

---
+++ DSA/Mergesort.java
@@ -0,0 +1,15 @@
+public class Mergesort {
+    void merge(int []a,int beg, int mid,int end){
+        int i,j,k;
+        int n1=mid-beg+1;
+        int n2=end-mid;
+        //temporary arrays
+        int leftarray[]=new int[n1];
+        int rightarray[]=new int[n2];
+
+        for(i=0;i<n1;i++){
+            leftarray[i]=a[beg+i];
+        }
+        fo
+    }
+}
