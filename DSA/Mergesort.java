f1c4381eb5112a4359f42e8fbedbd882367bb9a7
BMCBim <something123sdhgk@proton.me>
2023-07-27 11:50:27 +0545

    mergesort

--- DSA/Mergesort.java
+++ DSA/Mergesort.java
@@ -1,4 +1,4 @@
-public class Mergesort {
+public class mergeSort {
     void merge(int []a,int beg, int mid,int end){
         int i,j,k;
         int n1=mid-beg+1;
@@ -10,6 +10,61 @@ public class Mergesort {
         for(i=0;i<n1;i++){
             leftarray[i]=a[beg+i];
         }
-        fo
+        for(j=0;j<n2;j++){
+            rightarray[j]=a[mid+1+j];
+
+        }
+        i=0;
+        j=0;
+        k=beg;
+
+        while(i<n1 && j<n2){
+            if(leftarray[i]<=rightarray[j]){
+                a[k]=leftarray[i];
+                i++;
+            }
+        else{
+            a[k]=rightarray[j];
+            j++;
+        }
+        k++;
+        }
+        while(i<n1){
+            a[k]=leftarray[i];
+            j++;
+            k++;
+        }
+        while(j<n2){
+            a[k]=rightarray[j];
+            j++;
+            k++;
+        }
     }
+void mergeSort(int a[],int beg,int end){
+    if(beg<end){
+        int mid=(beg+end)/2;
+        mergeSort(a,mid,beg);
+        mergeSort(a,mid+1,end);
+        merge(a,beg,mid,end);
+    }
+}
+void printArray(int a[],int n){
+    int i;
+    for(i=0;i<n;i++){
+        System.out.println(a[i]+" ");
+    }
+}
+public static void main(String[] args) {
+    int a[]={11,22,33,44,55,66,77,88,99,00,44,22};
+    int n=a.length;
+    mergeSort m1= new mergeSort();
+    System.out.println("Before Sorting  the array");
+    m1.printArray(a,n);
+    m1.mergeSort(a,0,n-1);
+    System.out.println("Äfter aSorting the array");
+    m1.printArray(a,n);
+    System.out.println(" ");
+
+
+}
 }
